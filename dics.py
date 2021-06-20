0a663e96a97c7d5ca6c1abe0f988d467861972a4
Goma-dango <komi8.y@gmail.com>
2021-06-21 01:43:04 +0900

    update dics.py

--- dics.py
+++ dics.py
@@ -1,42 +1,17 @@
 # botが見るVTuberのリスト
-members = ['鳴尺よがる', '鬼城ある', '枕ちお', 'アンジー', '栗諏ひな']
-#members = ['鳴尺よがる', '四弓まこ', '鬼城ある', '枕ちお', 'アンジー', '栗諏ひな']
-#members = ['ごまだんご']
-
+members = ['NAME01', 'NAME02']
 
 # TwitterのURL
 # mobile版のURLにすること
-tw_url_dic = {
-    '鳴尺よがる': 'https://mobile.twitter.com/yogaru_ung',
-    # '四弓まこ': 'https://mobile.twitter.com/mako_ung',
-    '鬼城ある': 'https://mobile.twitter.com/aru_ung',
-    '枕ちお': 'https://mobile.twitter.com/chio_ung',
-    'アンジー': 'https://mobile.twitter.com/UNG_Angelina',
-    '栗諏ひな': 'https://mobile.twitter.com/chrishina_ung'
-}
-
-#tw_url_dic = {'ごまだんご': 'https://mobile.twitter.com/dangomushi564'}
+tw_url_dic = {'NAME01': 'NAME01_MOBILE_URL',
+              'NAME02': 'NAME02_MOBILE_URL'}
 
 
 # twitter idの辞書
-tw_id_dic = {
-    '鳴尺よがる': 1259703496946798592,
-    # '四弓まこ': 1263282176079978497,
-    '鬼城ある': 1281192269052145664,
-    '枕ちお': 1309823244661219328,
-    'アンジー': 1258343267525001217,
-    '栗諏ひな': 1373191526466023424
-}
-
-# tw_id_dic = {'ごまだんご': 761227335622209536}
+tw_id_dic = {'NAME01': 'NUMBER_01',  # twitter_id は予め確認しておく。別コードで見る。
+             'NAME02': 'NUMBER_02'}
 
 
 # カラーの辞書
-color_dic = {
-    '鳴尺よがる': [255, 240, 100],
-    # '四弓まこ': [255, 153, 153],
-    '鬼城ある': [255, 20, 90],
-    '枕ちお': [150, 240, 255],
-    'アンジー': [255, 0, 255],
-    '栗諏ひな': [255, 200, 200]
-}
+color_dic = {'NAME01': [255, 255, 255],
+             'NAME02': [0, 0, 0]}
